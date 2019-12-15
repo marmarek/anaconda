6186778acd90b5950a30bccebbcd0db7c0279b23
Frédéric Pierret (fepitre) <frederic.pierret@qubes-os.org>
2019-12-15 18:39:15 +0100

    Use installer kernel parameters as default for installed
    This way if any kernel parameter was need to boot Qubes on particular hardware, it will also be set to installed system
    
    Fixes QubesOS/qubes-issues#1650
    
    Adapted from a98ba275162937a56f7ca1ee5428b00de883677a

--- pyanaconda/modules/storage/bootloader/base.py
+++ pyanaconda/modules/storage/bootloader/base.py
@@ -920,11 +920,14 @@ class BootLoader(object):
 
     def _preserve_some_boot_args(self):
         """Preserve some of the boot args."""
-        for opt in conf.bootloader.preserved_arguments:
-            if opt not in kernel_arguments:
+        global_no_preserve_args = ["stage2", "root", "rescue", "rd.live.check",
+                                   "ip", "repo", "ks", "rd.md", "rd.luks",
+                                   "rd.dm", "rd.lvm.lv", "rd.lvm", "rd.neednet",
+                                   "rd.net.dhcp.vendor-class", "rd.driver.pre"]
+        for opt, arg in kernel_arguments.items():
+            if opt in global_no_preserve_args:
                 continue
 
-            arg = kernel_arguments.get(opt)
             new_arg = opt
             if arg:
                 new_arg += "=%s" % arg
