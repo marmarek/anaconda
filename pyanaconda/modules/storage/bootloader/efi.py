ebb81c1a54aa0dee1ac28f356884bfd2835d9e9e
Marek Marczykowski-Górecki <marmarek@invisiblethingslab.com>
2019-12-25 00:50:22 +0100

    Setup grubx64.efi directly as a bootloader
    Skip shim - we don't support SecureBoot and also Fedora's shim has
    hardcoded 'fedora' EFI vendor.

--- pyanaconda/modules/storage/bootloader/efi.py
+++ pyanaconda/modules/storage/bootloader/efi.py
@@ -146,8 +146,8 @@ class EFIBase(object):
 
 class EFIGRUB(EFIBase, GRUB2):
     """EFI GRUBv2"""
-    _packages32 = [ "grub2-efi-ia32", "shim-ia32" ]
-    _packages_common = ["efibootmgr", "grub2-tools", "grub2-tools-extra", "grubby" ]
+    _packages32 = [ "grub2-efi-ia32" ]
+    _packages_common = ["efibootmgr", "grub2-tools", "grub2-tools-extra" ]
     stage2_is_valid_stage1 = False
     stage2_bootable = False
 
@@ -155,7 +155,7 @@ class EFIGRUB(EFIBase, GRUB2):
 
     def __init__(self):
         super().__init__()
-        self._packages64 = [ "grub2-efi-x64", "shim-x64" ]
+        self._packages64 = [ "grub2-efi-x64" ]
 
         if self.get_fw_platform_size() == '32':
             self._is_32bit_firmware = True
@@ -163,8 +163,8 @@ class EFIGRUB(EFIBase, GRUB2):
     @property
     def _efi_binary(self):
         if self._is_32bit_firmware:
-            return "\\shimia32.efi"
-        return "\\shimx64.efi"
+            return "\\grubx32.efi"
+        return "\\grubx64.efi"
 
     @property
     def packages(self):
